37c2d35b5b7b5d16860f63d7c2fee4e3e5609ee6
rykymai <rykymai@gmail.com>
2018-10-26 15:02:51 +0200

    init

---
+++ Software/Fenice_BMS/Inc/ltc6813.h
@@ -0,0 +1,13 @@
+/*
+ * ltc6813.h
+ *
+ *  Created on: 26 ott 2018
+ *      Author: Utente
+ */
+
+#ifndef LTC6813_H_
+#define LTC6813_H_
+
+
+
+#endif /* LTC6813_H_ */
---
+++ Software/Fenice_BMS/Src/ltc6813.c
@@ -0,0 +1,8 @@
+/*
+ * ltc6813.c
+ *
+ *  Created on: 26 ott 2018
+ *      Author: Utente
+ */
+
+
