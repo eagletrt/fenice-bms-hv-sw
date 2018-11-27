620784488eadd9ac5d6626800d0e05960ad1bb7b
rykymai <rykymai@gmail.com>
2018-11-27 12:31:14 +0100

    ltc6813 impl

---
+++ Software/ltc6813test/stm32f7/Inc/ltc6813.h
@@ -0,0 +1,24 @@
+/*
+ * ltc6813.h
+ *
+ *  Created on: 27 nov 2018
+ *      Author: Utente
+ */
+
+#ifndef LTC6813_H_
+#define LTC6813_H_
+#include "stm32f7xx_hal.h"
+
+void ltc6813_adcv(uint8_t DCP, SPI_HandleTypeDef *hspi1);
+void wakeup_idle(SPI_HandleTypeDef *hspi1);
+void ltc6813_rdcv_voltages(uint8_t ic_n, uint16_t cell_voltages[18][2], SPI_HandleTypeDef *hspi1);
+void ltc6813_adax(uint8_t chg, SPI_HandleTypeDef *hspi1);
+void ltc6813_rdaux(uint8_t ic_n, uint16_t gpio_voltages[9][2],SPI_HandleTypeDef *hspi1);
+uint16_t pec15(uint8_t len,uint8_t data[],uint16_t crcTable[] );
+uint16_t convert_voltage(uint8_t v_data[]);
+void ltc6813_adstat( SPI_HandleTypeDef *hspi1);
+void ltc6804_rdstat(SPI_HandleTypeDef *hspi1,uint16_t stat_voltages[18][2] );
+void ltc6813_clrcell(SPI_HandleTypeDef *hspi1);
+void ltc6813_clraux(SPI_HandleTypeDef *hspi1);
+
+#endif /* LTC6813_H_ */
---
+++ Software/prova/teststmf3/Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.d
@@ -0,0 +1,89 @@
+Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.o: \
+ ../Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Inc/stm32f3xx_hal_conf.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Inc/main.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303x8.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h \
+ C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Inc/stm32f3xx_hal_conf.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Inc/main.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303x8.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h:
+
+C:/Users/Utente/Desktop/prova/teststmf3/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h:
