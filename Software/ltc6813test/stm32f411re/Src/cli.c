5272986cda434197f99d27b5acb3b5d1c308c2b3
Matteo Bonora <bonora.matteo@gmail.com>
2019-10-24 14:43:59 +0200

    Move stuff away from the CLI interrupt. Improve data visualization

--- Software/ltc6813test/stm32f411re/Src/cli.c
+++ Software/ltc6813test/stm32f411re/Src/cli.c
@@ -9,7 +9,7 @@ const char *cli_commands[N_COMMANDS] = {"volts", "temps", "status", "taba",
 char *_cli_volts(state_global_data_t *data, BMS_STATE_T state) {
 	char tmp[BUF_SIZE];
 
-	sprintf(tmp, "Total: %5.2f V\tMax: %4.2f V\tMin: %4.2f V\n",
+	sprintf(tmp, "Total.....%.2f V\nMax.......%.2f V\nMin.......%.2f V\n",
 			(float)data->pack.total_voltage / 10000,
 			(float)data->pack.max_voltage / 10000,
 			(float)data->pack.min_voltage / 10000);
@@ -21,7 +21,8 @@ char *_cli_volts(state_global_data_t *data, BMS_STATE_T state) {
 char *_cli_temps(state_global_data_t *data, BMS_STATE_T state) {
 	char tmp[BUF_SIZE];
 
-	sprintf(tmp, "Average: %4.1f °C\tMax: %4.1f °C\tMin: %4.1f °C\n",
+	sprintf(tmp,
+			"Average.....%.1f °C\nMax.........%.1f °C\nMin.........%.1f °C\n",
 			(float)data->pack.avg_temperature / 100,
 			(float)data->pack.max_temperature / 100,
 			(float)data->pack.min_temperature / 100);
@@ -31,20 +32,18 @@ char *_cli_temps(state_global_data_t *data, BMS_STATE_T state) {
 }
 
 char *_cli_status(state_global_data_t *data, BMS_STATE_T state) {
-	char *values[3][3] = {{"BMS state", (char *)bms_state_names[state]},
-						  {"Global error", (char *)error_names[data->error]},
-						  {"Global error index", NULL}};
-
 	char val[4];
 	itoa(data->error_index, val, 10);
-	values[2][1] = val;
+
+	char *values[3][3] = {{"BMS state", (char *)bms_state_names[state]},
+						  {"Global error", (char *)error_names[data->error]},
+						  {"Global error index", val}};
 
 	char tmp[BUF_SIZE] = "\0";
 
 	for (uint8_t i = 0; i < 3; i++) {
 		sprintf(tmp + strlen(tmp), "%s%s%s\n", values[i][0],
-				"........................" + strlen(values[i][0]),
-				values[i][1]);
+				"......................." + strlen(values[i][0]), values[i][1]);
 	}
 
 	char *out = tmp;
@@ -105,16 +104,41 @@ void cli_init(cli_t *cli, UART_HandleTypeDef *uart) {
 	HAL_UART_Transmit(cli->uart, (uint8_t *)init, strlen(init), 100);
 }
 
+void cli_clean(char *string, char *cmd) {
+	uint8_t cursor = 0;
+
+	// DON'T USE THE SAME VARIABLE FOR I/O
+
+	for (uint8_t i = 0; i < BUF_SIZE; i++) {
+		if (string[i] == '\0') {
+			return;
+		} else if (string[i] == '\b' && cursor > 0) {
+			// Check backspace
+			cursor--;
+			cmd[cursor] = '\0';
+		} else {
+			// Add to buffer
+			cmd[cursor] = string[i];
+			cursor++;
+		}
+	}
+
+	return;
+}
+
 void cli_loop(cli_t *cli, state_global_data_t *data, BMS_STATE_T state) {
 	if (cli->rx.complete) {
+		char cmd[BUF_SIZE];
+		cli_clean(cli->rx.buffer, cmd);
+
 		cli->rx.complete = false;
 
 		// char tmp[BUF_SIZE] = "?\n";
-		char *tmp = "?\n";
+		char *buf = "?\n";
 
 		for (uint8_t i = 0; i < N_COMMANDS; i++) {
-			if (strcmp(cli->rx.buffer, cli_commands[i]) == 0) {
-				tmp = cli->states[i](data, state);
+			if (strcmp(cmd, cli_commands[i]) == 0) {
+				buf = cli->states[i](data, state);
 			}
 		}
 
@@ -122,7 +146,7 @@ void cli_loop(cli_t *cli, state_global_data_t *data, BMS_STATE_T state) {
 			cli->rx.buffer[i] = '\0';
 		}
 
-		HAL_UART_Transmit(cli->uart, (uint8_t *)tmp, strlen(tmp), 100);
+		HAL_UART_Transmit(cli->uart, (uint8_t *)buf, strlen(buf), 100);
 
 		HAL_UART_Transmit(cli->uart, (uint8_t *)"> ", 2, 100);
 	}
@@ -133,23 +157,17 @@ void cli_char_receive(cli_t *cli) {
 	char rx_char = LL_USART_ReceiveData8(USART2);
 
 	if (rx_char == '\r' || rx_char == '\n') {
-		// Check new line
 		cli->rx.complete = true;
 		cli->rx.index = 0;
 		return;
-	} else if (rx_char == '\b' && cli->rx.index > 0) {
-		// Check backspace
-		cli->rx.buffer[--cli->rx.index] = '\0';
-	} else {
-		// Add to buffer
-		cli->rx.buffer[cli->rx.index] = rx_char;
-		cli->rx.index++;
 	}
 
-	/* Check if reception is completed (expected nb of bytes has been
-	 * received)
-	 */
+	// Add to buffer
+	cli->rx.buffer[cli->rx.index] = rx_char;
+	cli->rx.index++;
+
 	if (cli->rx.index == BUF_SIZE) {
 		cli->rx.complete = true;
+		cli->rx.index = 0;
 	}
 }
