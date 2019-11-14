14f916abd85001b365d925a759923c1ae95e398c
Matteo Bonora <bonora.matteo@gmail.com>
2019-11-14 15:40:21 +0100

    Add initial i2c code

---
+++ temperature.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(115200);
+	Wire.begin(69);
+	Wire.setClock(400000);
+	Wire.onReceive(receiveEvent);
+
+	Serial.println("Ready");
+}
+
+void loop() {}
+
+void receiveEvent(int howMany) {
+	digitalWrite(LED_BUILTIN, HIGH);
+
+	while (Wire.available()) {  // loop through all but the last
+		char c = Wire.read();   // receive byte as a character
+		Serial.println(c);		// print the character
+	}
+	Wire.write("dc");
+	digitalWrite(LED_BUILTIN, LOW);
+}
