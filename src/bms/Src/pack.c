0365ce0985a9e50763042007bd3221527bc1a670
Matteo Bonora <bonora.matteo@gmail.com>
2019-11-28 11:00:57 +0100

    Add FAT support

--- src/bms/Src/pack.c
+++ src/bms/Src/pack.c
@@ -114,23 +114,23 @@ End:;
 uint8_t pack_update_temperatures(SPI_HandleTypeDef *spi, PACK_T *pack,
 								 ERROR_T *error) {
 	uint8_t send[8] = {0};
-	/*ltc6813_wrcomm_i2c_w(spi, 69, 0x41);
+	ltc6813_wrcomm_i2c_w(spi, 69, 0x41);
 
 	if (!ltc6813_rdcomm_i2c(spi, send)) {
 		cli_print("pecw\r\n", 8);
 	}
-	ltc6813_stcomm_i2c(spi, 3);*/
+	ltc6813_stcomm_i2c(spi, 3);
 	// HAL_Delay(1);
 	///// read
 	uint8_t recv[8] = {0};
-	ltc6813_wrcomm_i2c_r(spi, 69);
+	/*ltc6813_wrcomm_i2c_r(spi, 69);
 	ltc6813_stcomm_i2c(spi, 4);
 
 	// HAL_Delay(1);
 
 	if (!ltc6813_rdcomm_i2c(spi, recv)) {
 		cli_print("pecr\r\n", 8);
-	}
+	}*/
 
 	// uint8_t icom0 = recv[0] >> 4;
 	uint8_t d0 = (recv[0] << 4) | (recv[1] >> 4);
