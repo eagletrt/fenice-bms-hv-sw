461b9178bf6e13a1ddb1f1fb66c73d0f6309fc87
Bonnee <bonora.matteo@gmail.com>
2019-12-16 18:05:54 +0100

    Update ltc1865

--- src/bms/Src/comm/ltc1865l.c
+++ src/bms/Src/comm/ltc1865l.c
@@ -21,11 +21,17 @@ uint16_t ltc1865l_read_voltage(SPI_HandleTypeDef *spi, GPIO_TypeDef *GPIOx,
 	cmd[0] |= single << 7;
 	cmd[0] |= odd << 6;
 
+	uint32_t start = HAL_GetTick();
+
+	// Start measurement
+	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
+	// Generate a rising edge
 	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
 	HAL_Delay(1);
+
 	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(spi, cmd, 2, 50);
-	HAL_SPI_Receive(spi, data, 2, 50);
+	HAL_SPI_Transmit(spi, cmd, 2, 10);
+	HAL_SPI_Receive(spi, data, 2, 10);
 	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
 
 	return _ltc1865l_convert_voltage(data);
