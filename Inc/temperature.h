c98dae5de90ef91bbc6213d7f86820bbbd65a9d7
Bonnee <bonora.matteo@gmail.com>
2020-02-22 15:50:06 +0100

    Fix temperature measurement

--- Inc/temperature.h
+++ Inc/temperature.h
@@ -13,7 +13,26 @@
 #include "../../fenice_config.h"
 #include "stm32f4xx_hal.h"
 
-void temperature_read(I2C_HandleTypeDef *hi2c[TEMP_BUS_COUNT], uint16_t *temps);
+typedef struct temperature {
+	uint8_t values[TEMP_SENSOR_COUNT];
 
+	uint8_t min[2];
+	uint8_t max[2];
+} temperature_t;
+
+void temperature_read_sample(I2C_HandleTypeDef *hi2c[TEMP_BUS_COUNT],
+							 uint16_t *buffer);
+
+void temperature_get_average(
+	uint16_t buffer[TEMP_SENSOR_COUNT][TEMP_SAMPLE_COUNT],
+	uint8_t temps[TEMP_SENSOR_COUNT]);
+
+/**
+ * @brief Computes the two highest and lowest temperatures
+ *
+ * @param		temps		The input array
+ * @param		min			The lowest temperature output array
+ * @param		max			The highest temperature output array
+ */
 void temperature_get_extremes(uint8_t temps[], uint8_t min[2], uint8_t max[2]);
 #endif
