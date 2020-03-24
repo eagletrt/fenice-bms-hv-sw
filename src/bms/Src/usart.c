5ea547589415a815d2015c3c32f808205d9e3c90
Bonnee <bonora.matteo@gmail.com>
2020-03-24 08:35:33 +0100

    Partially fix cli freezes

--- src/bms/Src/usart.c
+++ src/bms/Src/usart.c
@@ -29,169 +29,145 @@ UART_HandleTypeDef huart3;
 
 /* USART2 init function */
 
-void MX_USART2_UART_Init(void)
-{
-
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 57600;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
+void MX_USART2_UART_Init(void) {
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+	huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_8_8) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_8_8) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_EnableFifoMode(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 /* USART3 init function */
 
-void MX_USART3_UART_Init(void)
-{
-
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 500000;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart3.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetTxFifoThreshold(&huart3, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetRxFifoThreshold(&huart3, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_DisableFifoMode(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
+void MX_USART3_UART_Init(void) {
+	huart3.Instance = USART3;
+	huart3.Init.BaudRate = 500000;
+	huart3.Init.WordLength = UART_WORDLENGTH_8B;
+	huart3.Init.StopBits = UART_STOPBITS_1;
+	huart3.Init.Parity = UART_PARITY_NONE;
+	huart3.Init.Mode = UART_MODE_TX_RX;
+	huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart3.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+	huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart3) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetTxFifoThreshold(&huart3, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetRxFifoThreshold(&huart3, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_DisableFifoMode(&huart3) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
-void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
-{
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle) {
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	if (uartHandle->Instance == USART2) {
+		/* USER CODE BEGIN USART2_MspInit 0 */
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(uartHandle->Instance==USART2)
-  {
-  /* USER CODE BEGIN USART2_MspInit 0 */
+		/* USER CODE END USART2_MspInit 0 */
+		/* USART2 clock enable */
+		__HAL_RCC_USART2_CLK_ENABLE();
 
-  /* USER CODE END USART2_MspInit 0 */
-    /* USART2 clock enable */
-    __HAL_RCC_USART2_CLK_ENABLE();
-  
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USART2 GPIO Configuration    
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**USART2 GPIO Configuration    
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+		GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /* USER CODE BEGIN USART2_MspInit 1 */
+		/* USER CODE BEGIN USART2_MspInit 1 */
 
-  /* USER CODE END USART2_MspInit 1 */
-  }
-  else if(uartHandle->Instance==USART3)
-  {
-  /* USER CODE BEGIN USART3_MspInit 0 */
+		/* USER CODE END USART2_MspInit 1 */
+	} else if (uartHandle->Instance == USART3) {
+		/* USER CODE BEGIN USART3_MspInit 0 */
 
-  /* USER CODE END USART3_MspInit 0 */
-    /* USART3 clock enable */
-    __HAL_RCC_USART3_CLK_ENABLE();
-  
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**USART3 GPIO Configuration    
+		/* USER CODE END USART3_MspInit 0 */
+		/* USART3 clock enable */
+		__HAL_RCC_USART3_CLK_ENABLE();
+
+		__HAL_RCC_GPIOB_CLK_ENABLE();
+		/**USART3 GPIO Configuration    
     PB10     ------> USART3_TX
     PB11     ------> USART3_RX 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+		GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* USER CODE BEGIN USART3_MspInit 1 */
+		/* USER CODE BEGIN USART3_MspInit 1 */
 
-  /* USER CODE END USART3_MspInit 1 */
-  }
+		/* USER CODE END USART3_MspInit 1 */
+	}
 }
 
-void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
-{
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle) {
+	if (uartHandle->Instance == USART2) {
+		/* USER CODE BEGIN USART2_MspDeInit 0 */
 
-  if(uartHandle->Instance==USART2)
-  {
-  /* USER CODE BEGIN USART2_MspDeInit 0 */
+		/* USER CODE END USART2_MspDeInit 0 */
+		/* Peripheral clock disable */
+		__HAL_RCC_USART2_CLK_DISABLE();
 
-  /* USER CODE END USART2_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_USART2_CLK_DISABLE();
-  
-    /**USART2 GPIO Configuration    
+		/**USART2 GPIO Configuration    
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX 
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
+		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
 
-    /* USART2 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(USART2_IRQn);
-  /* USER CODE BEGIN USART2_MspDeInit 1 */
+		/* USART2 interrupt Deinit */
+		HAL_NVIC_DisableIRQ(USART2_IRQn);
+		/* USER CODE BEGIN USART2_MspDeInit 1 */
 
-  /* USER CODE END USART2_MspDeInit 1 */
-  }
-  else if(uartHandle->Instance==USART3)
-  {
-  /* USER CODE BEGIN USART3_MspDeInit 0 */
+		/* USER CODE END USART2_MspDeInit 1 */
+	} else if (uartHandle->Instance == USART3) {
+		/* USER CODE BEGIN USART3_MspDeInit 0 */
 
-  /* USER CODE END USART3_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_USART3_CLK_DISABLE();
-  
-    /**USART3 GPIO Configuration    
+		/* USER CODE END USART3_MspDeInit 0 */
+		/* Peripheral clock disable */
+		__HAL_RCC_USART3_CLK_DISABLE();
+
+		/**USART3 GPIO Configuration    
     PB10     ------> USART3_TX
     PB11     ------> USART3_RX 
     */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
+		HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10 | GPIO_PIN_11);
 
-  /* USER CODE BEGIN USART3_MspDeInit 1 */
+		/* USER CODE BEGIN USART3_MspDeInit 1 */
 
-  /* USER CODE END USART3_MspDeInit 1 */
-  }
-} 
+		/* USER CODE END USART3_MspDeInit 1 */
+	}
+}
 
 /* USER CODE BEGIN 1 */
 
@@ -199,84 +175,9 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
 	cli_handle_interrupt();
 }
 
-// /**
-//  * @brief  Rx Transfer completed callback
-//  * @note   This example shows a simple way to report end of IT Rx transfer,
-//  * and you can add your own implementation.
-//  * @retval None
-//  */
+void HAL_UART_ErrorCallback(UART_HandleTypeDef* huart) {
+}
 
-// void UART_CharReception_Callback(void) {
-// 	/* Read Received character. RXNE flag is cleared by reading of DR
-// 	 * register
-// 	 */
-
-// 	// NICE STUFF BRO
-// 	cli_char_receive();
-// }
-
-// // DONT NEED THIS BULLSHIT DOWN THERE
-
-// /**
-//  * @brief  Function called for achieving next TX Byte sending
-//  * @retval None
-//  */
-// void UART_TXEmpty_Callback(void) {
-// 	// if (tx_buf.index == (ubSizeToSend - 1)) {
-// 	/* Disable TXE interrupt */
-// 	LL_USART_DisableIT_TXE(USART2);
-
-// 	/* Enable TC interrupt */
-// 	LL_USART_EnableIT_TC(USART2);
-// 	//}
-
-// 	/* Fill DR with a new char */
-// 	// LL_USART_TransmitData8(USART2, aTxStartMessage[rx_buf.index++]);
-// }
-
-// /**
-//  * @brief  Function called at completion of last byte transmission
-//  * @retval None
-//  */
-// void UART_CharTransmitComplete_Callback(void) {
-// 	// if (tx_buf.index == sizeof(aTxStartMessage)) {
-// 	// 	tx_buf.index = 0;
-
-// 	// 	/* Disable TC interrupt */
-// 	LL_USART_DisableIT_TC(USART2);
-
-// 	// 	/* Set Tx complete boolean to 1 */
-// 	// 	tx_complete = 1;
-// 	// }
-// }
-
-// /**
-//  * @brief  UART error callbacks
-//  * @note   This example shows a simple way to report transfer error, and you
-//  * can add your own implementation.
-//  * @retval None
-//  */
-// void UART_Error_Callback(void) {
-// 	//__IO uint32_t sr_reg;
-
-// 	/* Disable USARTx_IRQn */
-// 	NVIC_DisableIRQ(USART2_IRQn);
-
-// 	/* Error handling example :
-// 	  - Read USART SR register to identify flag that leads to IT raising
-// 	  - Perform corresponding error handling treatment according to flag
-// 	*/
-// 	//sr_reg = LL_USART_ReadReg(USART2, SR);
-// 	//if (sr_reg & LL_USART_ISR_NE) {
-// 	/* Turn LED2 off: Transfer error in reception/transmission process
-// 		 */
-// 	// BSP_LED_Off(LED2);
-// 	//} else {
-// 	/* Turn LED2 off: Transfer error in reception/transmission process
-// 		 */
-// 	// BSP_LED_Off(LED2);
-// 	//}
-// }
 /* USER CODE END 1 */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
