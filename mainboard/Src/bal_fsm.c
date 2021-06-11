2c03c48ae00988e968572ee58b3281b31db77067
Bonnee <bonora.matteo@gmail.com>
2021-06-11 12:19:08 +0200

    config: implement config handler for balancing

--- mainboard/Src/bal_fsm.c
+++ mainboard/Src/bal_fsm.c
@@ -10,83 +10,97 @@
 #include "bal_fsm.h"
 
 #include "cli_bms.h"
+#include "config.h"
 #include "fenice_config.h"
 #include "ltc6813_utils.h"
 #include "pack.h"
 #include "spi.h"
 
-fsm bal_fsm;
+#define CONF_VER  0x01
+#define CONF_ADDR 0x01
 
-uint16_t threshold = BAL_MAX_VOLTAGE_THRESHOLD;
+typedef struct {
+    uint8_t version;
+    voltage_t threshold;
+} bal_params;
+bal_params bal_params_default = {CONF_VER, BAL_MAX_VOLTAGE_THRESHOLD};
+
+fsm bal_fsm;
+config_t config;
+bal_params bal_conf_params;
+
 uint32_t cycle_length = BAL_CYCLE_LENGTH;
 
 uint32_t discharge_start = 0;
 
-uint16_t trans_stop(fsm* FSM);
-uint16_t do_off(fsm* FSM);
-uint16_t do_compute(fsm* FSM);
-uint16_t do_discharge(fsm* FSM);
-uint16_t do_cooldown(fsm* FSM);
+uint16_t trans_stop(fsm *FSM);
+uint16_t do_off(fsm *FSM);
+uint16_t do_compute(fsm *FSM);
+uint16_t do_discharge(fsm *FSM);
+uint16_t do_cooldown(fsm *FSM);
 
-uint16_t bal_get_threshold() {
-	return threshold;
+voltage_t bal_get_threshold() {
+    return *(voltage_t *)config_get(&config);
 }
 
 void bal_set_threshold(uint16_t thresh) {
-	threshold = thresh;
+    config_set(&config, &thresh);
+    config_write(&config);
 }
 
 void bal_fsm_init() {
-	fsm_init(&bal_fsm, BAL_NUM_STATES);
+    fsm_init(&bal_fsm, BAL_NUM_STATES);
 
-	bal_fsm.state_table[BAL_OFF][BAL_OFF] = &do_off;
-	bal_fsm.state_table[BAL_OFF][BAL_COMPUTE] = &do_compute;
+    bal_fsm.state_table[BAL_OFF][BAL_OFF]     = &do_off;
+    bal_fsm.state_table[BAL_OFF][BAL_COMPUTE] = &do_compute;
 
-	bal_fsm.state_table[BAL_COMPUTE][BAL_DISCHARGE] = &do_discharge;
-	bal_fsm.state_table[BAL_COMPUTE][BAL_OFF] = &trans_stop;
+    bal_fsm.state_table[BAL_COMPUTE][BAL_DISCHARGE] = &do_discharge;
+    bal_fsm.state_table[BAL_COMPUTE][BAL_OFF]       = &trans_stop;
 
-	bal_fsm.state_table[BAL_DISCHARGE][BAL_DISCHARGE] = &do_discharge;
-	bal_fsm.state_table[BAL_DISCHARGE][BAL_COMPUTE] = &do_compute;
-	bal_fsm.state_table[BAL_DISCHARGE][BAL_OFF] = &trans_stop;
+    bal_fsm.state_table[BAL_DISCHARGE][BAL_DISCHARGE] = &do_discharge;
+    bal_fsm.state_table[BAL_DISCHARGE][BAL_COMPUTE]   = &do_compute;
+    bal_fsm.state_table[BAL_DISCHARGE][BAL_OFF]       = &trans_stop;
 
-	bal_fsm.state_table[BAL_COOLDOWN][BAL_COOLDOWN] = &do_cooldown;
-	bal_fsm.state_table[BAL_COOLDOWN][BAL_COMPUTE] = &do_compute;
-	bal_fsm.state_table[BAL_COOLDOWN][BAL_OFF] = &trans_stop;
+    bal_fsm.state_table[BAL_COOLDOWN][BAL_COOLDOWN] = &do_cooldown;
+    bal_fsm.state_table[BAL_COOLDOWN][BAL_COMPUTE]  = &do_compute;
+    bal_fsm.state_table[BAL_COOLDOWN][BAL_OFF]      = &trans_stop;
+
+    config_init(&config, CONF_ADDR, &bal_params_default, sizeof(bal_params));
 }
 
-uint16_t trans_stop(fsm* FSM) {
-	uint16_t cells[PACK_CELL_COUNT] = {0};
-	ltc6813_set_balancing(&LTC6813_PERIPHERAL, cells, 0);
-	return BAL_OFF;
+uint16_t trans_stop(fsm *FSM) {
+    uint16_t cells[PACK_CELL_COUNT] = {0};
+    ltc6813_set_balancing(&LTC6813_PERIPHERAL, cells, 0);
+    return BAL_OFF;
 }
-uint16_t do_off(fsm* FSM) {
-	return BAL_OFF;
-}
-
-uint16_t do_compute(fsm* FSM) {
-	uint16_t cells[PACK_CELL_COUNT];
-
-	if (bal_get_cells_to_discharge(pack_get_voltages(), PACK_CELL_COUNT, threshold, cells) != 0) {
-		ltc6813_set_balancing(&LTC6813_PERIPHERAL, cells, cycle_length);
-		discharge_start = HAL_GetTick();
-
-		cli_bms_debug("Discharging cells", 18);
-		return BAL_DISCHARGE;
-	}
-	cli_bms_debug("Non si può fare meglio di così.", 34);
-	return BAL_OFF;
+uint16_t do_off(fsm *FSM) {
+    return BAL_OFF;
 }
 
-uint16_t do_discharge(fsm* FSM) {
-	if (discharge_start - HAL_GetTick() >= cycle_length) {
-		return BAL_COOLDOWN;
-	}
-	return BAL_DISCHARGE;
+uint16_t do_compute(fsm *FSM) {
+    uint16_t cells[PACK_CELL_COUNT];
+
+    if (bal_get_cells_to_discharge(pack_get_voltages(), PACK_CELL_COUNT, bal_get_threshold(), cells) != 0) {
+        ltc6813_set_balancing(&LTC6813_PERIPHERAL, cells, cycle_length);
+        discharge_start = HAL_GetTick();
+
+        cli_bms_debug("Discharging cells", 18);
+        return BAL_DISCHARGE;
+    }
+    cli_bms_debug("Non si può fare meglio di così.", 34);
+    return BAL_OFF;
 }
 
-uint16_t do_cooldown(fsm* FSM) {
-	if (discharge_start - HAL_GetTick() >= cycle_length + BAL_COOLDOWN_DELAY) {
-		return BAL_COMPUTE;
-	}
-	return BAL_COOLDOWN;
+uint16_t do_discharge(fsm *FSM) {
+    if (discharge_start - HAL_GetTick() >= cycle_length) {
+        return BAL_COOLDOWN;
+    }
+    return BAL_DISCHARGE;
+}
+
+uint16_t do_cooldown(fsm *FSM) {
+    if (discharge_start - HAL_GetTick() >= cycle_length + BAL_COOLDOWN_DELAY) {
+        return BAL_COMPUTE;
+    }
+    return BAL_COOLDOWN;
 }
--- mainboard/Src/main.c
+++ mainboard/Src/main.c
@@ -51,8 +51,6 @@
 /* USER CODE BEGIN PV */
 DMA_HandleTypeDef hdma_adc1;
 
-m95256_t eeprom;
-
 uint32_t timer_volts = 0;
 uint32_t timer_temps = 0;
 /* USER CODE END PV */
@@ -67,23 +65,23 @@ static void MX_NVIC_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 void check_timers() {
-	uint32_t tick = HAL_GetTick();
+    uint32_t tick = HAL_GetTick();
 
-	// Read and send temperatures
-	if (tick - timer_temps >= TEMPS_READ_INTERVAL) {
-		timer_temps = tick;
+    // Read and send temperatures
+    if (tick - timer_temps >= TEMPS_READ_INTERVAL) {
+        timer_temps = tick;
 
-		read_temps();
-		can_send(ID_HV_CURRENT);
-	}
+        read_temps();
+        can_send(ID_HV_CURRENT);
+    }
 
-	// Read and send voltages and current
-	if (tick - timer_volts >= VOLTS_READ_INTERVAL) {
-		timer_volts = tick;
+    // Read and send voltages and current
+    if (tick - timer_volts >= VOLTS_READ_INTERVAL) {
+        timer_volts = tick;
 
-		read_volts();
-		can_send(ID_HV_VOLTAGE);
-	}
+        read_volts();
+        can_send(ID_HV_VOLTAGE);
+    }
 }
 
 /**
@@ -91,161 +89,155 @@ void check_timers() {
  * @details It runs voltage and current measurements
  */
 void read_volts() {
-	// Voltages
-	pack_update_voltages(&hspi1, &huart3);
+    // Voltages
+    pack_update_voltages(&hspi1, &huart3);
 
-	// Current
-	pack_update_current();
+    // Current
+    pack_update_current();
 }
 
 void read_temps() {
-	// Temperatures
-	pack_update_temperatures(&hspi1);
+    // Temperatures
+    pack_update_temperatures(&hspi1);
 }
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void) {
-	/* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick.
-   */
-	HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-	/* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+    /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_SPI1_Init();
-	MX_SPI2_Init();
-	MX_USART2_UART_Init();
-	MX_USART3_UART_Init();
-	MX_TIM2_Init();
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_SPI1_Init();
+    MX_SPI2_Init();
+    MX_USART2_UART_Init();
+    MX_USART3_UART_Init();
+    MX_TIM2_Init();
 
-	/* Initialize interrupts */
-	MX_NVIC_Init();
-	/* USER CODE BEGIN 2 */
-	FDCAN1_Init();
-	HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
-	HAL_FDCAN_Start(&hfdcan1);
+    /* Initialize interrupts */
+    MX_NVIC_Init();
+    /* USER CODE BEGIN 2 */
 
-	m95256_init(&eeprom, &spi_eeprom, CS_EEPROM_GPIO_Port, CS_EEPROM_Pin);
+    FDCAN1_Init();
+    HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
+    HAL_FDCAN_Start(&hfdcan1);
 
-	config_load();
+    error_init();
+    fsm_bms_init();
+    bal_fsm_init();
+    cli_bms_init();
+    can_init();
 
-	error_init();
-	fsm_bms_init();
-	bal_fsm_init();
-	cli_bms_init();
-	can_init();
+    if (si8900_init(&huart3, ADC_SIN_GPIO_Port, ADC_SIN_Pin)) {
+        cli_print(&cli_bms, "SI8900 INITIALIZED\r\n", 20);
+    } else {
+        cli_print(&cli_bms, "SI8900 ERROR\r\n", 14);
+    }
 
-	if (si8900_init(&huart3, ADC_SIN_GPIO_Port, ADC_SIN_Pin)) {
-		cli_print(&cli_bms, "SI8900 INITIALIZED\r\n", 20);
-	} else {
-		cli_print(&cli_bms, "SI8900 ERROR\r\n", 14);
-	}
+    /* USER CODE END 2 */
 
-	/* USER CODE END 2 */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        /* USER CODE END WHILE */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
-	while (1) {
-		/* USER CODE END WHILE */
-
-		/* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
 		fsm_run(&fsm_bms);
 		fsm_run(&bal_fsm);
 
-		check_timers();
+        check_timers();
 
-		cli_loop(&cli_bms);
-	}
-	return 0;
-	/* USER CODE END 3 */
+        cli_loop(&cli_bms);
+    }
+    return 0;
+    /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct   = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct   = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-	/** Configure the main internal regulator output voltage
-   */
-	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
-	/** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
-	RCC_OscInitStruct.PLL.PLLN = 85;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
-	/** Initializes the CPU, AHB and APB buses clocks
-   */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-								  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Configure the main internal regulator output voltage
+  */
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+    /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM            = RCC_PLLM_DIV4;
+    RCC_OscInitStruct.PLL.PLLN            = 85;
+    RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ            = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR            = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+  */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
-		Error_Handler();
-	}
-	/** Initializes the peripherals clocks
-   */
-	PeriphClkInit.PeriphClockSelection =
-		RCC_PERIPHCLK_USART2 | RCC_PERIPHCLK_USART3 | RCC_PERIPHCLK_FDCAN;
-	PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-	PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
-	PeriphClkInit.FdcanClockSelection = RCC_FDCANCLKSOURCE_PLL;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
-		Error_Handler();
-	}
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the peripherals clocks
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2 | RCC_PERIPHCLK_USART3 | RCC_PERIPHCLK_FDCAN;
+    PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+    PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+    PeriphClkInit.FdcanClockSelection  = RCC_FDCANCLKSOURCE_PLL;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /**
- * @brief NVIC Configuration.
- * @retval None
- */
+  * @brief NVIC Configuration.
+  * @retval None
+  */
 static void MX_NVIC_Init(void) {
-	/* FDCAN1_IT0_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(FDCAN1_IT0_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(FDCAN1_IT0_IRQn);
-	/* FDCAN1_IT1_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(FDCAN1_IT1_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(FDCAN1_IT1_IRQn);
-	/* USART2_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(USART2_IRQn);
+    /* FDCAN1_IT0_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(FDCAN1_IT0_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(FDCAN1_IT0_IRQn);
+    /* FDCAN1_IT1_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(FDCAN1_IT1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(FDCAN1_IT1_IRQn);
+    /* USART2_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
 }
 
 /* USER CODE BEGIN 4 */
@@ -253,32 +245,32 @@ static void MX_NVIC_Init(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return
    * state
    */
 
-	/* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line) {
-	/* USER CODE BEGIN 6 */
-	/* User can add his own implementation to report the file name and line
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line
      number, tex: printf("Wrong parameters value: file %s on line %d\r\n",
      file, line) */
-	/* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
