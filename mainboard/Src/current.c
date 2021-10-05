2b15ec0ece1803a70372af96d1925b532192d9f4
Bonnee <bonora.matteo@gmail.com>
2021-10-05 20:34:34 +0200

    current: move zero adjust after rolling average

--- mainboard/Src/current.c
+++ mainboard/Src/current.c
@@ -18,11 +18,11 @@ current_t current[CURRENT_SENSOR_NUM] = {0.f};
 current_t zero[CURRENT_SENSOR_NUM]    = {0.f};
 
 current_t _current_convert_low(float volt) {
-    return ((1 / 40.0e-3f) * (volt / (330.f / (330 + 169)) - 2.5f) - zero[CURRENT_SENSOR_50]);
+>    return ((1 / 40.0e-3f) * (volt / (330.f / (330 + 169)) - 2.5f);
 }
 
 current_t _current_convert_high(float volt) {
-    return ((1 / 2.5e-3f) * (volt / (330.f / (330 + 169)) - 2.5f) - zero[CURRENT_SENSOR_300]);
+    return ((1 / 2.5e-3f) * (volt / (330.f / (330 + 169)) - 2.5f);
 }
 
 current_t _current_convert_shunt() {
@@ -30,6 +30,7 @@ current_t _current_convert_shunt() {
 }
 
 uint32_t current_measure() {
+    // TODO: use DMA to read and make rolling average directly on ADC values (look at conversion speed and target around 200Hz)
     // Start ADC measurements for every sensor
     HAL_ADC_Start(&hadc1);
     HAL_ADC_Start(&hadc2);
@@ -42,14 +43,16 @@ uint32_t current_measure() {
     // Convert Hall-low (50A) and save rolling average
     uint16_t adcval            = HAL_ADC_GetValue(&hadc1);
     float volt                 = (adcval * 3.3f) / 4095;
-    current[CURRENT_SENSOR_50] = ROLLING_AVERAGE_FACTOR * _current_convert_low(volt) +
-                                 (1 - ROLLING_AVERAGE_FACTOR) * current[CURRENT_SENSOR_50];
+    current[CURRENT_SENSOR_50] = (ROLLING_AVERAGE_FACTOR * _current_convert_low(volt) +
+                                  (1 - ROLLING_AVERAGE_FACTOR) * current[CURRENT_SENSOR_50]) -
+                                 zero[CURRENT_SENSOR_50];
 
     // Convert Hall-high (300A) and save rolling average
     adcval                      = HAL_ADC_GetValue(&hadc2);
     volt                        = (adcval * 3.3f) / 4095;
-    current[CURRENT_SENSOR_300] = ROLLING_AVERAGE_FACTOR * _current_convert_high(volt) +
-                                  (1 - ROLLING_AVERAGE_FACTOR) * current[CURRENT_SENSOR_300];
+    current[CURRENT_SENSOR_300] = (ROLLING_AVERAGE_FACTOR * _current_convert_high(volt) +
+                                   (1 - ROLLING_AVERAGE_FACTOR) * current[CURRENT_SENSOR_300]) -
+                                  zero[CURRENT_SENSOR_300];
 
     // Check for over-current
     error_toggle_check(current_get_current() > PACK_MAX_CURRENT, ERROR_OVER_CURRENT, 0);
