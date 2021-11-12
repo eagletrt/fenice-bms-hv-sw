af141b06261308dfa0f2e40a983f4f69a44e5fc3
Federico Carbone <federico.carbone20@gmail.com>
2021-11-12 16:57:18 +0100

    fixed fenice_config.h after merge

--- fenice_config.h
+++ fenice_config.h
@@ -34,23 +34,95 @@
 #define CELLBOARD_CELL_COUNT 18
 
 //===========================================================================
+//=================================== LTC6813 ===============================
+//===========================================================================
+
+#define LTC6813_PERIPHERAL hspi1
+
+// Set to 1 to emulate the LTC daisy chain
+#define LTC6813_EMU 1
+
+/**
+ * Number of daisy chained LTCs
+ */
+#define LTC6813_COUNT 6
+
+/**
+ * Number of cells a single IC controls. Refer to cell_distribution for
+ * configuration
+ */
+#define LTC6813_CELL_COUNT 18
+
+/**
+ * Number of registers for each LTC
+ */
+#define LTC6813_REG_COUNT 6
+
+/**
+ * Number of cells handled by a register
+ */
+#define LTC6813_REG_CELL_COUNT 3
+
+#define LTC6813_TEMP_ADDRESS 69
+
+//===========================================================================
+//================================= Temperature =============================
+//===========================================================================
+
+/**
+ * Temperature measurement interval (ms)
+ */
+#define TEMP_READ_INTERVAL 100
+
+/**
+ * How many temperatures to average together
+ */
+#define TEMP_SAMPLE_COUNT 4
+
+/**
+ * How many I2C buses are used to address all the sensors
+ */
+#define TEMP_BUS_COUNT 1
+
+/**
+ * How many strips in each bus
+ */
+#define TEMP_STRIPS_PER_BUS 1
+/**
+ * How many sensors are on a strip
+ */
+#define TEMP_SENSORS_PER_STRIP 6
+
+/**
+ * How many sensors on each cellboard
+ */
+#define TEMP_SENSOR_COUNT (TEMP_BUS_COUNT * TEMP_STRIPS_PER_BUS * TEMP_SENSORS_PER_STRIP)
+
+/**
+ * The address pin coding for each sensor in a strip. The LSB must be changed
+ * from [0-2] to differentiate between each strip.
+ */
+static const uint8_t TEMP_SENSOR_ADDRESS_CODING[TEMP_SENSORS_PER_STRIP] = {000, 100, 010, 110, 020, 120};
+
+//===========================================================================
 //================================ Pack Settings ============================
 //===========================================================================
 
 /**
  * Total number of cells in series
  */
-#define PACK_CELL_COUNT (CELLBOARD_COUNT * CELLBOARD_CELL_COUNT)
+#define PACK_CELL_COUNT (LTC6813_COUNT * LTC6813_CELL_COUNT)
 
 /**
  * How many temperature sensors in the pack
  */
-#define PACK_TEMP_COUNT (TEMP_SENSOR_COUNT * CELLBOARD_COUNT)
+#define PACK_TEMP_COUNT (TEMP_SENSOR_COUNT * LTC6813_COUNT)
 
 /**
  * Max current. (A)
  */
 #define PACK_MAX_CURRENT 180.0f
+
 
 /**
  * Cell's limit voltages (mV * 10)
@@ -96,10 +168,6 @@
 #define PRECHARGE_TIMEOUT           10000U
 #define PRECHARGE_CHECK_INTERVAL    100U
 #define PRECHARGE_VOLTAGE_THRESHOLD 0.95
-
-typedef uint16_t voltage_t;
-typedef float temperature_t;
-typedef int16_t current_t;
 
 /**
  * Feedback bit set bit position 
