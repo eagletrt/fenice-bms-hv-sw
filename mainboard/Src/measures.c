0320eb8f41edee1a66533f7513bc6603c9c756ef
fedebone00 <fedebone00@hotmail.it>
2021-11-18 18:26:18 +0100

    measures: use of a flag for current and temp measurements

--- mainboard/Src/measures.c
+++ mainboard/Src/measures.c
@@ -1,12 +1,14 @@
 #include "measures.h"
 
+bool measure_current_flag=false, measure_temp_flag=false;
+
 void measures_init() {
     HAL_TIM_OC_Start_IT(&HTIM_MEASURES, TIM_CHANNEL_1);
     HAL_TIM_OC_Start_IT(&HTIM_MEASURES, TIM_CHANNEL_2);
 }
 
 void measures_current() {
-    //voltage_measure(&SI8900_UART);
+    voltage_measure(&SI8900_UART);
     current_read();
     soc_sample_energy(HAL_GetTick());
 }
