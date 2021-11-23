8a0a4a0446f9d12f23fb1b960bdaf81cb73cc1c3
fedebone00 <fedebone00@hotmail.it>
2021-11-23 12:07:16 +0100

    can: handled empty balancing.cells array as stop signal

--- cellboard/Core/Src/bal_fsm.c
+++ cellboard/Core/Src/bal_fsm.c
@@ -8,12 +8,13 @@
  */
 
 #include "bal_fsm.h"
-#include "bal.h"
 #include "can_comms.h"
 #include "ltc6813_utils.h"
 #include "main.h"
 #include "spi.h"
 #include "volt.h"
+
+#include <string.h>
 
 bal_fsm bal;
 
@@ -47,10 +48,9 @@ void transition_callback(fsm handle) {
 }
 
 void off_entry(fsm handle) {
-    bms_balancing_cells cells = bms_balancing_cells_default;
-    bal.cells_length                = 0;
+    bms_balancing_cells cells = { 1, 1, 1 };
 
-    ltc6813_set_balancing(&LTC6813_SPI, cells, PACK_CELL_COUNT, 0);
+    ltc6813_set_balancing(&LTC6813_SPI, cells, 0);
 }
 
 void off_handler(fsm handle, uint8_t event) {
@@ -62,7 +62,7 @@ void off_handler(fsm handle, uint8_t event) {
 }
 
 void discharge_entry(fsm handle) {
-    ltc6813_set_balancing(&LTC6813_SPI, bal.cells, bal.cells_length, bal.cycle_length);
+    ltc6813_set_balancing(&LTC6813_SPI, bal.cells, bal.cycle_length);
     //  Resetting and starting the DISCHARGE_TIMER
     HAL_TIM_Base_Stop_IT(&DISCHARGE_TIMER);
     __HAL_TIM_SetCounter(&DISCHARGE_TIMER, 0U);
@@ -86,4 +86,10 @@ void bal_timers_handler(TIM_HandleTypeDef* htim, fsm handle){
     if(htim->Instance == DISCHARGE_TIMER.Instance){
         fsm_trigger_event(bal.fsm, EV_BAL_STOP);
     }
+}
+
+uint8_t bal_is_cells_empty() {
+    bms_balancing_cells empty = bms_balancing_cells_default;
+    volatile int a = memcmp(bal.cells, &empty, sizeof(bal.cells)) == 0;
+    return a;
 }
