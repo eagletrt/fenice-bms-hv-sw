5908f66641a4cc75f976cb0b77ff8f08ee17c1ed
Federico Carbone <federico.carbone20@gmail.com>
2022-04-06 17:58:07 +0200

    cellboard: implemented measurements flag as bitset and added ts_status sending message to 200ms timer (with temps)

--- cellboard/Core/Inc/measurements.h
+++ cellboard/Core/Inc/measurements.h
@@ -8,6 +8,14 @@
 #define VOLTS_READ_ACTIVE_CHANNEL               HAL_TIM_ACTIVE_CHANNEL_2
 #define TEMPS_READ_ACTIVE_CHANNEL               HAL_TIM_ACTIVE_CHANNEL_3
 
+typedef uint8_t measurements_flag_t;
+
+enum {
+    MEASUREMENTS_VOLTS_START_CONVERTION_FLAG = 1,
+    MEASUREMENTS_VOLTS_READ_FLAG = 2,
+    MEASUREMENTS_TEMPS_READ_FLAG = 4
+};
+
 void measurements_init(TIM_HandleTypeDef *htim);
 
 void measurements_flags_check();
--- cellboard/Core/Src/measurements.c
+++ cellboard/Core/Src/measurements.c
@@ -3,7 +3,7 @@
 #include "temp.h"
 #include "tim.h"
 
-bool volts_start_conv_flag=false, volts_read_flag=false, temps_read_flag=false;
+measurements_flag_t flags;
 
 void measurements_init(TIM_HandleTypeDef *htim) {
     __HAL_TIM_SetCompare(htim, VOLTS_START_CONVERTION_CHANNEL, TIM_MS_TO_TICKS(htim, VOLT_MEASURE_INTERVAL - VOLT_MEASURE_TIME));
@@ -12,42 +12,44 @@ void measurements_init(TIM_HandleTypeDef *htim) {
     HAL_TIM_OC_Start_IT(htim, VOLTS_START_CONVERTION_CHANNEL);
     HAL_TIM_OC_Start_IT(htim, VOLTS_READ_CHANNEL);
     HAL_TIM_OC_Start_IT(htim, TEMPS_READ_CHANNEL);
+    flags = 0;
 }
 
 void measurements_flags_check() {
-    if(volts_start_conv_flag) {
+    if(flags & MEASUREMENTS_VOLTS_START_CONVERTION_FLAG) {
         volt_start_measure();
-        volts_start_conv_flag = false;
+        flags &= ~MEASUREMENTS_VOLTS_START_CONVERTION_FLAG;
     }
-    if(volts_read_flag) {
+    if(flags & MEASUREMENTS_VOLTS_READ_FLAG) {
         volt_read();
         can_send(TOPIC_VOLTAGE_INFO_FILTER);
-        volts_read_flag = false;
+        flags &= ~MEASUREMENTS_VOLTS_READ_FLAG;
     }
-    if(temps_read_flag) {
+    if(flags & MEASUREMENTS_TEMPS_READ_FLAG) {
         temp_measure_all();
         can_send(TOPIC_TEMPERATURE_INFO_FILTER);
-        temps_read_flag = false;
+        can_send(TOPIC_STATUS_FILTER);
+        flags &= ~MEASUREMENTS_TEMPS_READ_FLAG;
     }
 }
 
 void measurements_oc_handler(TIM_HandleTypeDef *htim) {
-    uint32_t compare_value = __HAL_TIM_GetCounter(htim);
+    uint32_t cnt = __HAL_TIM_GetCounter(htim);
     switch (htim->Channel)
     {
         case VOLTS_START_CONVERTION_ACTIVE_CHANNEL:
-            volts_start_conv_flag = true;
+            flags |= MEASUREMENTS_VOLTS_START_CONVERTION_FLAG;
             break;
 
         case VOLTS_READ_ACTIVE_CHANNEL:
-            volts_read_flag = true;
-            __HAL_TIM_SetCompare(htim, VOLTS_START_CONVERTION_CHANNEL, compare_value + TIM_MS_TO_TICKS(htim, VOLT_MEASURE_INTERVAL - VOLT_MEASURE_TIME));
-            __HAL_TIM_SetCompare(htim, VOLTS_READ_CHANNEL, compare_value + TIM_MS_TO_TICKS(htim, VOLT_MEASURE_INTERVAL));
+            __HAL_TIM_SetCompare(htim, VOLTS_START_CONVERTION_CHANNEL, cnt + TIM_MS_TO_TICKS(htim, VOLT_MEASURE_INTERVAL - VOLT_MEASURE_TIME));
+            __HAL_TIM_SetCompare(htim, VOLTS_READ_CHANNEL, cnt + TIM_MS_TO_TICKS(htim, VOLT_MEASURE_INTERVAL));
+            flags |= MEASUREMENTS_VOLTS_READ_FLAG;
             break;
         
         case TEMPS_READ_ACTIVE_CHANNEL:
-            __HAL_TIM_SetCompare(htim, TEMPS_READ_CHANNEL, compare_value + TIM_MS_TO_TICKS(htim, TEMP_MEASURE_INTERVAL));
-            temps_read_flag = true;
+            __HAL_TIM_SetCompare(htim, TEMPS_READ_CHANNEL, cnt + TIM_MS_TO_TICKS(htim, TEMP_MEASURE_INTERVAL));
+            flags |= MEASUREMENTS_TEMPS_READ_FLAG;
             break;
 
         default:
