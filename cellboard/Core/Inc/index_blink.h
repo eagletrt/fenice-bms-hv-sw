4ae05a04404624259f82d2ae94d4421dbbf7705f
fedebone00 <fedebone00@hotmail.it>
2022-04-06 17:19:28 +0200

    cellboard: added async blinking

---
+++ cellboard/Core/Inc/index_blink.h
@@ -0,0 +1,7 @@
+#include <string.h>
+#include "blink.h"
+
+#define index_blink_run() blink_run(&led_blink);
+
+extern blink_t led_blink;
+void index_blink_init(GPIO_TypeDef *led_port, uint16_t led_pin, bool repeat);
