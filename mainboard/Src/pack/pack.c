056073cf8085d06a91e4ac7c0ed1c736cfda3619
fedebone00 <fedebone00@hotmail.it>
2022-04-27 15:39:21 +0200

    pack: ordered things up

--- mainboard/Src/pack/pack.c
+++ mainboard/Src/pack/pack.c
@@ -10,41 +10,29 @@
 
 #include "feedback.h"
 
-void pack_set_ts_on() {
-    HAL_GPIO_WritePin(TS_ON_GPIO_Port, TS_ON_Pin, GPIO_PIN_SET);
+void pack_set_ts_on(uint8_t value) {
+    HAL_GPIO_WritePin(TS_ON_GPIO_Port, TS_ON_Pin, value);
 }
 
 void pack_set_airn_off(uint8_t value) {
     HAL_GPIO_WritePin(AIRN_OFF_GPIO_Port, AIRN_OFF_Pin, value);
 }
 
-void pack_set_pc_on() {
-    //HAL_GPIO_WritePin(PC_ON)
+void pack_set_precharge(uint8_t value) {
+    HAL_GPIO_WritePin(PRECHARGE_GPIO_Port, PRECHARGE_Pin, value);
 }
 
-bool pack_set_ts_off() {
-    //Switch off airs
-    HAL_GPIO_WritePin(TS_ON_GPIO_Port, TS_ON_Pin, GPIO_PIN_RESET);
-
-    //feedback_read(FEEDBACK_TS_OFF_MASK);
-    return 0;//feedback_check(FEEDBACK_TS_OFF_MASK, FEEDBACK_TS_OFF_VAL, ERROR_FEEDBACK);
-}
-
-void pack_set_pc_start() {
-    HAL_GPIO_WritePin(PRECHARGE_GPIO_Port, PRECHARGE_Pin, GPIO_PIN_SET);
-
-    /*
-    // Check feedback
-    feedback_read(FEEDBACK_TO_PRECHARGE_MASK);
-    return feedback_check(FEEDBACK_TO_PRECHARGE_MASK, FEEDBACK_TO_PRECHARGE_VAL, ERROR_FEEDBACK);
-    */
-}
-
-void pack_set_precharge_end() {
-    //switch on AIR+
-    HAL_GPIO_WritePin(AIRP_OFF_GPIO_Port, AIRP_OFF_Pin, GPIO_PIN_RESET);
+void pack_set_airp_off(uint8_t value) {
+    HAL_GPIO_WritePin(AIRP_OFF_GPIO_Port, AIRP_OFF_Pin, value);
 }
 
 void pack_set_fault(uint8_t value) {
     HAL_GPIO_WritePin(BMS_FAULT_GPIO_Port, BMS_FAULT_Pin, value);
+}
+
+void pack_set_default_off() {
+    pack_set_ts_on(TS_OFF_VALUE);
+    pack_set_airn_off(AIRN_OFF_VALUE);
+    pack_set_airp_off(AIRP_OFF_VALUE);
+    pack_set_precharge(PRECHARGE_OFF_VALUE);
 }
