18dbe3b94c1d995d3a5462e91df0632754c5f3f0
fedebone00 <fedebone00@hotmail.it>
2022-04-27 15:45:22 +0200

    can_comm: update can-cicd and added check on cellboards presence

--- mainboard/Inc/peripherals/can_comm.h
+++ mainboard/Inc/peripherals/can_comm.h
@@ -16,12 +16,19 @@
 #include "pack/voltage.h"
 
 
+#define CAN_1MBIT_PRE 3
+#define CAN_1MBIT_BS1 CAN_BS1_12TQ
+#define CAN_1MBIT_BS2 CAN_BS2_2TQ
+
+#define CAN_125KBIT_PRE 20
+#define CAN_125KBIT_BS1 CAN_BS1_15TQ
+#define CAN_125KBIT_BS2 CAN_BS2_2TQ
+
+typedef enum { CAN_BITRATE_1MBIT, CAN_BITRATE_125KBIT } CAN_Bitrate;
 
 #define CAN_WAIT(C)                                                                                     \
     {                                                                                                   \
-        uint32_t tick = HAL_GetTick();                                                                  \
-        while (HAL_GetTick() - tick < 10 &&                                                             \
-                HAL_CAN_GetTxMailboxesFreeLevel(C) == 0);                                               \
+        while (HAL_CAN_GetTxMailboxesFreeLevel(C) == 0);                                                \
     } 
 
 #define CAN_SLAVE_START_FILTER_BANK 14
@@ -32,3 +39,5 @@ void can_tx_header_init();
 HAL_StatusTypeDef can_send(CAN_HandleTypeDef *hcan, uint8_t *buffer, CAN_TxHeaderTypeDef *header);
 HAL_StatusTypeDef can_car_send(uint16_t id);
 HAL_StatusTypeDef can_bms_send(uint16_t id);
+void can_cellboards_check();
+void CAN_change_bitrate(CAN_HandleTypeDef *hcan, CAN_Bitrate bitrate);
