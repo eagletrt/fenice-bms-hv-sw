e65cc3f85af98ed54ef8b93a3a26851b23bc0878
Federico Carbone <federico.carbone20@gmail.com>
2022-05-09 16:54:02 +0200

    feedback: fully implemented logic; TODO: set feedbacks masks

--- mainboard/Inc/feedback.h
+++ mainboard/Inc/feedback.h
@@ -1,52 +1,84 @@
 /**
- * @file		feedback.h
- * @brief		Feedback parsing utilities
+ * @file	feedback.h
+ * @brief	Feedback parsing utilities
  *
- * @date		Mar 16, 2021
+ * @date	Mar 16, 2021
  * @author      Matteo Bonora [matteo.bonora@studenti.unitn.it]
+ * @author  	Federico Carbone [federico.carbone@studenti.unitn.it]
  */
 
 #pragma once
 
 #include "error.h"
-
-#include <inttypes.h>
 #include "fenice_config.h"
 
-#define MUX_INTERVAL_MS 0.1
-#define MUX_DELAY_MS    0.01
+#include <inttypes.h>
 
-#define FB_CHECK_INTERVAL   2
-#define FB_TIMEOUT    50
+#define MUX_INTERVAL_MS 0.1
+
+#define FB_CHECK_INTERVAL_MS 2
+#define FB_TIMEOUT_MS        50
 
 typedef uint32_t feedback_t;
 
-extern feedback_t feedback;
+extern uint64_t feedback;
+
+#define FEEDBACK_TS_OFF_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_TS_OFF_LOW FEEDBACK_NULL
+
+#define FEEDBACK_TS_OFF_VAL  FEEDBACK_TS_OFF_HIGH
+#define FEEDBACK_TS_OFF_MASK (FEEDBACK_TS_OFF_HIGH | FEEDBACK_TS_OFF_LOW)
+
+#define FEEDBACK_TS_ON_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_TS_ON_LOW FEEDBACK_NULL
+
+#define FEEDBACK_TS_ON_VAL  FEEDBACK_TS_ON_HIGH
+#define FEEDBACK_TS_ON_MASK (FEEDBACK_TS_ON_HIGH | FEEDBACK_TS_ON_LOW)
+
+#define FEEDBACK_AIRN_CLOSE_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_AIRN_CLOSE_LOW FEEDBACK_NULL
+
+#define FEEDBACK_AIRN_CLOSE_VAL  FEEDBACK_AIRN_CLOSE_HIGH
+#define FEEDBACK_AIRN_CLOSE_MASK (FEEDBACK_AIRN_CLOSE_HIGH | FEEDBACK_AIRN_CLOSE_LOW)
+
+#define FEEDBACK_AIRN_STATUS_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_AIRN_STATUS_LOW FEEDBACK_NULL
+
+#define FEEDBACK_AIRN_STATUS_VAL  FEEDBACK_AIRN_STATUS_HIGH
+#define FEEDBACK_AIRN_STATUS_MASK (FEEDBACK_AIRN_STATUS_HIGH | FEEDBACK_AIRN_STATUS_LOW)
+
+#define FEEDBACK_PC_ON_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_PC_ON_LOW FEEDBACK_NULL
+
+#define FEEDBACK_PC_ON_VAL  FEEDBACK_PC_ON_HIGH
+#define FEEDBACK_PC_ON_MASK (FEEDBACK_PC_ON_HIGH | FEEDBACK_PC_ON_LOW)
+
+#define FEEDBACK_ON_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_ON_LOW FEEDBACK_NULL
+
+#define FEEDBACK_ON_VAL  FEEDBACK_ON_HIGH
+#define FEEDBACK_ON_MASK (FEEDBACK_ON_HIGH | FEEDBACK_ON_LOW)
+
+#define FEEDBACK_FAULT_EXIT_HIGH FEEDBACK_CHECK_MUX
+
+#define FEEDBACK_FAULT_EXIT_LOW FEEDBACK_NULL
+
+#define FEEDBACK_FAULT_EXIT_VAL  FEEDBACK_FAULT_EXIT_HIGH
+#define FEEDBACK_FAULT_EXIT_MASK (FEEDBACK_FAULT_EXIT_HIGH | FEEDBACK_FAULT_EXIT_LOW)
 
 /*
-// Feedback states before closing AIR- and trigger PC
-#define FEEDBACK_IDLE_TS_ON_TRIGGER_VAL                                                                    \
-    (FEEDBACK_VREF | FEEDBACK_FROM_TSMS | FEEDBACK_TO_TSMS | FEEDBACK_FROM_SHUTDOWN | FEEDBACK_LATCH_IMD | \
-     FEEDBACK_LATCH_BMS | FEEDBACK_IMD_FAULT | FEEDBACK_BMS_FAULT)
-#define FEEDBACK_IDLE_TS_ON_TRIGGER_MASK FEEDBACK_ALL
-
-// Feedback states after closing AIR-
-#define FEEDBACK_TO_PRECHARGE_VAL                                                                          \
-    (FEEDBACK_IDLE_TS_ON_TRIGGER_VAL | FEEDBACK_AIR_NEGATIVE | FEEDBACK_RELAY_LV | FEEDBACK_IMD_SHUTDOWN | \
-     FEEDBACK_BMS_FAULT | FEEDBACK_BMS_SHUTDOWN | FEEDBACK_TS_ON)
-#define FEEDBACK_TO_PRECHARGE_MASK FEEDBACK_ALL &(~FEEDBACK_TSAL_HV)
-
-//#define FEEDBACK_ON_VAL  FEEDBACK_TO_PRECHARGE_VAL | FEEDBACK_TSAL_HV
-*/
-
-
-
 // Feedbacks to check after TS OFF
 #define FEEDBACK_TS_OFF_VAL \
-        FEEDBACK_CHECK_MUX| FEEDBACK_TSAL_GREEN
+        FEEDBACK_CHECK_MUX| FEEDBACK_TSAL_GREEN | FEEDBACK_AIRN_STATUS
 
 #define FEEDBACK_TS_OFF_MASK \
-        FEEDBACK_TS_OFF_VAL /*| FEEDBACK_PC_END*/ | FEEDBACK_AIRN_GATE | FEEDBACK_AIRP_GATE
+        FEEDBACK_TS_OFF_VAL /*| FEEDBACK_PC_END | FEEDBACK_AIRN_GATE | FEEDBACK_AIRP_GATE | FEEDBACK_AIRN_STATUS
 
 
 
@@ -78,21 +110,16 @@ extern feedback_t feedback;
         FEEDBACK_CHECK_MUX | FEEDBACK_AIRP_GATE | FEEDBACK_AIRN_GATE
         
 #define FEEDBACK_ON_MASK FEEDBACK_ALL
+*/
 
+#define FEEDBACK_CONVERT_ADC_TO_VOLTAGE(VALUE) ((VALUE) * (3.3F / 4095 * 4.3f))
 
-
-#define CONVERT_ADC_TO_VOLTAGE(VALUE) ((VALUE) * 3.3F / 4096)
-
+typedef enum { FEEDBACK_STATE_H, FEEDBACK_STATE_L, FEEDBACK_STATE_ERROR } FEEDBACK_STATE;
 
 void feedback_init();
-feedback_t feedback_check(feedback_t fb_check_mask, feedback_t fb_value, error_id error_id);
-void feedback_read(feedback_t fb_mask);
-bool feedback_check_charge();
-bool feedback_check_precharge();
-bool feedback_check_on();
+void feedback_get_feedback_states(FEEDBACK_STATE *out_value);
+feedback_t feedback_check(feedback_t fb_check_mask, feedback_t fb_value);
 
-uint8_t feedback_get_mux_index();
 void feedback_set_next_mux_index();
-void feedback_save_mux_value(uint32_t adc_value, uint8_t index);
 void _feedback_handle_tim_elapsed_irq();
 void _feedback_handle_adc_cnv_cmpl_irq();
--- mainboard/Src/feedback.c
+++ mainboard/Src/feedback.c
@@ -4,135 +4,136 @@
  *
  * @date		Mar 16, 2021
  * @author  	Matteo Bonora [matteo.bonora@studenti.unitn.it]
+ * @author  	Federico Carbone [federico.carbone@studenti.unitn.it]
  */
 
 #include "feedback.h"
 
+#include "adc.h"
 #include "main.h"
 #include "mainboard_config.h"
-#include "adc.h"
-#include "cli_bms.h"
+#include "pack/pack.h"
 #include "tim.h"
 
-const float feedback_analog_threshold_h = 10.5f / 4.3f;
-const float feedback_analog_threshold_l = 1.5f / 4.3f;
-feedback_t feedback;
-uint16_t adc_values[ADC_VALUES_COUNT] = {0};
-uint8_t fb_index = FEEDBACK_MUX_N-1;
-uint16_t dma_data[ADC_VALUES_COUNT-FEEDBACK_MUX_N+1];
+#define feedback_analog_threshold_h    11.0f
+#define feedback_analog_threshold_l    0.2f
+#define feedback_check_mux_threshold_h 3.4f
+#define feedback_check_mux_threshold_l 3.2f
+
+uint16_t feedback_adc_values[FEEDBACK_N] = {0};
+uint8_t fb_index                         = FEEDBACK_MUX_N - 1;
+uint16_t dma_data[FEEDBACK_N - FEEDBACK_MUX_N + 1];
 
 void feedback_init() {
-    feedback = 0;
-    __HAL_TIM_SET_AUTORELOAD(&HTIM_MUX, TIM_MS_TO_TICKS(&HTIM_MUX, MUX_INTERVAL_MS));
-    //__HAL_TIM_SET_COMPARE(&HTIM_MUX, TIM_CHANNEL_1, TIM_MS_TO_TICKS(&HTIM_MUX, MUX_DELAY_MS));
     __HAL_TIM_CLEAR_IT(&HTIM_MUX, TIM_IT_UPDATE);
-    __HAL_TIM_CLEAR_FLAG(&HTIM_MUX, TIM_FLAG_CC1);
-    //HAL_TIM_OC_Start_IT(&HTIM_MUX, TIM_CHANNEL_1);
+    __HAL_TIM_SET_AUTORELOAD(&HTIM_MUX, TIM_MS_TO_TICKS(&HTIM_MUX, MUX_INTERVAL_MS));
     HAL_TIM_Base_Start_IT(&HTIM_MUX);
-
-    HAL_GPIO_WritePin(BMS_FAULT_GPIO_Port, BMS_FAULT_Pin, GPIO_PIN_SET);
 }
 
 bool _is_adc_value_high(uint8_t index) {
-    float val = CONVERT_ADC_TO_VOLTAGE(adc_values[index]);
-    if(index == FEEDBACK_CHECK_MUX_POS) val *= 4.3F;
+    float val = FEEDBACK_CONVERT_ADC_TO_VOLTAGE(feedback_adc_values[index]);
 
     return val > feedback_analog_threshold_h;
 }
 
 bool _is_adc_value_low(uint8_t index) {
-    float val = CONVERT_ADC_TO_VOLTAGE(adc_values[index]);
-    if(index == FEEDBACK_CHECK_MUX_POS) val *= 4.3F;
+    float val = FEEDBACK_CONVERT_ADC_TO_VOLTAGE(feedback_adc_values[index]);
+
+    if (index >= FEEDBACK_MUX_N)
+        val /= 4.3f;
 
     return val < feedback_analog_threshold_l;
 }
 
-feedback_t feedback_check(feedback_t fb_check_mask, feedback_t fb_value, error_id error_id) {
-    feedback_t difference = 0;
-    
-    for (uint8_t i=0; i<FEEDBACK_N; ++i) {
-        feedback_t fb = fb_value & (1U << i);
-        if((fb_check_mask & (1U << i))) {
-            if(i<ADC_VALUES_COUNT) {
-                if(((fb && _is_adc_value_high(i)) || (!fb && _is_adc_value_low(i)))) {
-                    error_reset(error_id, i);
-                } else {
-                    error_set(error_id, i, HAL_GetTick());
-                    difference |= 1U << i;
-                }
+bool _is_adc_value_valid(uint8_t index) {
+    return _is_adc_value_high(index) || _is_adc_value_low(index);
+}
+
+bool _is_check_mux_ok() {
+    float val = FEEDBACK_CONVERT_ADC_TO_VOLTAGE(feedback_adc_values[FEEDBACK_CHECK_MUX_POS]);
+    return val > feedback_check_mux_threshold_l && val < feedback_check_mux_threshold_h;
+}
+
+void feedback_get_feedback_states(FEEDBACK_STATE *out_value) {
+    for (uint8_t i = 0; i < FEEDBACK_N; ++i) {
+        feedback_t fb_i = 1U << i;
+
+        if (fb_i == FEEDBACK_CHECK_MUX) {
+            if (_is_check_mux_ok()) {
+                out_value[i] = FEEDBACK_STATE_H;
             } else {
-                error_toggle_check(((feedback & fb_check_mask) ^ fb_value) & (1U << i), error_id, i);
+                out_value[i] = FEEDBACK_STATE_ERROR;
             }
-        } else {
-            error_reset(error_id, i);
+            continue;
+        }
+
+        if (_is_adc_value_valid(i)) {     //if the adc value is valid
+            if (_is_adc_value_high(i)) {  //adc value is H
+                out_value[i] = FEEDBACK_STATE_H;
+            } else {  //adc value is L
+                out_value[i] = FEEDBACK_STATE_L;
+            }
+        } else {  //adc value is not valid, circuitry error
+            out_value[i] = FEEDBACK_STATE_ERROR;
+        }
+    }
+}
+
+feedback_t feedback_check(feedback_t fb_check_mask, feedback_t fb_value) {
+    feedback_t differences = 0;
+
+    for (uint8_t i = 0; i < FEEDBACK_N; ++i) {
+        feedback_t fb_i     = 1U << i;
+        feedback_t fb_i_val = fb_value & fb_i;
+
+        if (fb_i == FEEDBACK_CHECK_MUX) {
+            if (_is_check_mux_ok()) {
+                error_reset(ERROR_FEEDBACK_CIRCUITRY, i);
+            } else {
+                error_set(ERROR_FEEDBACK_CIRCUITRY, i, HAL_GetTick());
+                differences |= fb_i;
+            }
+            continue;
+        }
+
+        if (fb_check_mask & fb_i) {        //if the feedback is in the check mask
+            if (_is_adc_value_valid(i)) {  //if the adc value is valid
+                if ((fb_i_val && _is_adc_value_high(i)) ||
+                    (!fb_i_val && _is_adc_value_low(i))) {  //adc value is as expected (H or L)
+                    error_reset(ERROR_FEEDBACK, i);
+                } else {  //adc value is not as expected
+                    error_set(ERROR_FEEDBACK, i, HAL_GetTick());
+                    differences |= fb_i;
+                }
+                error_reset(ERROR_FEEDBACK_CIRCUITRY, i);
+            } else {  //adc value is not valid, circuitry error
+                error_set(ERROR_FEEDBACK_CIRCUITRY, i, HAL_GetTick());
+                differences |= fb_i;
+            }
+        } else {  //then reset the error
+            error_reset(ERROR_FEEDBACK, i);
         }
     }
 
-    return difference;
+    return differences;
 }
 
-// //this check is performed during ON state and from PRECHARGE TO ON
-// bool pack_feedback_check_on() {
-// 	uint16_t difference = pd_feedback ^ FEEDBACK_ON;
-
-// 	for (uint8_t i = 0; i < FEEDBACK_N; i++) {
-// 		error_toggle_check(difference & (1 << i), ERROR_FEEDBACK_HARD, i);
-// 	}
-
-// 	return pd_feedback == FEEDBACK_ON;
-// }
-
-// bool pack_feedback_check_charge() {
-// 	uint16_t difference = pd_feedback ^ FEEDBACK_CHARGE;
-
-// 	for (uint8_t i = 0; i < FEEDBACK_N; i++) {
-// 		error_toggle_check(difference & (1 << i), ERROR_FEEDBACK_HARD, i);
-// 	}
-
-// 	return pd_feedback == FEEDBACK_CHARGE;
-// }
-
 void feedback_set_next_mux_index() {
-    fb_index = (fb_index+1) % FEEDBACK_MUX_N;
+    fb_index = (fb_index + 1) % FEEDBACK_MUX_N;
     HAL_GPIO_WritePin(MUX_A0_GPIO_Port, MUX_A0_Pin, (fb_index & 0b00000001));
     HAL_GPIO_WritePin(MUX_A1_GPIO_Port, MUX_A1_Pin, (fb_index & 0b00000010));
     HAL_GPIO_WritePin(MUX_A2_GPIO_Port, MUX_A2_Pin, (fb_index & 0b00000100));
     HAL_GPIO_WritePin(MUX_A3_GPIO_Port, MUX_A3_Pin, (fb_index & 0b00001000));
 }
 
-void feedback_save_mux_value(uint32_t adc_value, uint8_t index) {
-    adc_values[index] = adc_value;
-}
-
-uint8_t feedback_get_mux_index() {
-    return fb_index;
-}
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-    feedback_t fb = 0;
-    GPIO_TypeDef *GPIO_Port;
-    switch(GPIO_Pin) {
-        case FB_SD_END_Pin:
-            GPIO_Port = FB_SD_END_GPIO_Port;
-            fb = FEEDBACK_SD_END;
-            break;
-        default: return;
-    }
-
-    if (HAL_GPIO_ReadPin(GPIO_Port, GPIO_Pin)) {
-        feedback |= fb;
-    } else {
-        feedback &= ~fb;
-    }
-}
-
 void _feedback_handle_tim_elapsed_irq() {
-    HAL_ADC_Start_DMA(&ADC_MUX, (uint32_t*)dma_data, sizeof(dma_data));
+    HAL_ADC_Start_DMA(&ADC_MUX, (uint32_t *)dma_data, FEEDBACK_N - FEEDBACK_MUX_N + 1);
 }
 
 void _feedback_handle_adc_cnv_cmpl_irq() {
-    adc_values[fb_index] = dma_data[0];
-    adc_values[ADC_VALUES_COUNT-2] = dma_data[1]; //FB_RELAY_SD
-    adc_values[ADC_VALUES_COUNT-1] = dma_data[2]; //FB_IMD_FAULT
+    feedback_adc_values[fb_index]               = dma_data[0];
+    feedback_adc_values[FEEDBACK_RELAY_SD_POS]  = dma_data[1];  //FB_RELAY_SD
+    feedback_adc_values[FEEDBACK_IMD_FAULT_POS] = dma_data[2];  //FB_IMD_FAULT
+    feedback_adc_values[FEEDBACK_SD_END_POS]    = dma_data[3];
     feedback_set_next_mux_index();
 }
