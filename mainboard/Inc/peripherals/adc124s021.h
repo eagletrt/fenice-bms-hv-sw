e5a7dc43976afbbafe4694d7ef0199d8744bf9c2
Federico Carbone <federico.carbone20@gmail.com>
2022-06-23 17:33:28 +0200

    dynamically calculated shunt_offset at startup

--- mainboard/Inc/peripherals/adc124s021.h
+++ mainboard/Inc/peripherals/adc124s021.h
@@ -15,6 +15,6 @@
 
 typedef enum { ADC124S_CH1 = 0x00, ADC124S_CH2, ADC124S_CH3, ADC124S_CH4 } ADC124S021_CH;
 
-voltage_t adc124S021_read_channel(SPI_HandleTypeDef *spi, ADC124S021_CH channel);
+float adc124S021_read_channel(SPI_HandleTypeDef *spi, ADC124S021_CH channel);
 
 bool adc124s021_read_channels(SPI_HandleTypeDef *spi, ADC124S021_CH *channels, uint8_t ch_number, float *data_out);
